2729b59050cc785ed4287314e41839a910c8b13f
Eduardo Ferreira Jaworiwski <89814995+eduardoferreirajaworiwski@users.noreply.github.com>
2023-05-31 20:23:51 -0300

    Add files

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,35 @@
+package principal;
+
+import javax.swing.JOptionPane;
+
+import calculadora.Calculadora;
+
+public class Principal {
+    public static void main(String[] args) {
+        String opcao = "";
+        Calculadora calculadora = new Calculadora();
+        while(!opcao.equals("9")){
+            opcao = JOptionPane.showInputDialog("1 - Leitura \n2 - Soma \n3 - Subtração"+"\n4 - Produto \n5 - Divisão \n9 - Sair");
+            switch(Integer.parseInt(opcao)){
+                case 1:
+                calculadora.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de A")));
+                calculadora.setValorB(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de B")));
+                break;
+                case 2:
+                JOptionPane.showMessageDialog(null, "Soma: "+calculadora.getAdicao());
+                break;
+                case 3:
+                JOptionPane.showMessageDialog(null, "Soma: "+calculadora.getSubtracao());
+                case 4:
+                JOptionPane.showMessageDialog(null, "Soma: "+calculadora.getProduto());
+                break;
+                case 5:
+                JOptionPane.showMessageDialog(null, "Soma: "+calculadora.getDivisao());
+                break;
+
+
+            }
+        }
+
+    }
+}
